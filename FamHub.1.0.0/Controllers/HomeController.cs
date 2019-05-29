3390dbc2bceccb7d776da577f896624fc16ebada
Mayeze Showee <showeemayeze@outlook.fr>
2019-05-29 16:15:56 +0200

    creation du controlleurs Utilisateurs et de la vue Acceuil

--- FamHub.1.0.0/Controllers/HomeController.cs
+++ FamHub.1.0.0/Controllers/HomeController.cs
@@ -10,34 +10,11 @@ namespace FamHub._1._0._0.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public ViewResult Index()
         {
-            return View();
+            return View("Acceuil");
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        
     }
 }
---
+++ FamHub.1.0.0/Models/Utilisateurs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FamHub._1._0._0.Models
+{
+    public class Utilisateurs
+    {
+        public string Nom { get; set; }
+        public string PostNom { get; set; }
+        public string Prenom { get; set; }
+        public DateTime DateDeNaissance { get; set; }
+        public string Pseudo { get; set; }
+        public string Email { get; set; }
+        public string NumeroDeTelephone { get; set; }
+        public string NomFamille { get; set; }
+           
+            
+    }
+}
