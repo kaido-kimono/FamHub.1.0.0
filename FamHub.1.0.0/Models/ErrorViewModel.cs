8e08bc7224d232cbe4467c4d5c6c4da66a7f6a15
Mayeze Showee <showeemayeze@outlook.fr>
2019-05-29 12:23:43 +0200

    Ajoutez des fichiers projet.

---
+++ FamHub.1.0.0/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FamHub._1._0._0.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
